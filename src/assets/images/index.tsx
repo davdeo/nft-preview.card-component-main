3b1749e2e08b781b4ce6159f9274e266c023bad4
david.monz <david.monz@macio.de>
2021-12-22 15:28:05 +0100

    Add and import assets

---
+++ src/assets/images/index.tsx
@@ -0,0 +1,17 @@
+import IconClock from './icon-clock.svg';
+import IconEthereum from './icon-ethereum.svg';
+import IconView from './icon-view.svg';
+
+import ImageAvatar from './image-avatar.png';
+import ImageEquilibrium from './image-equilibrium.jpg';
+
+export const ICON = {
+    CLOCK: IconClock,
+    ETHEREUM: IconEthereum,
+    VIEW: IconView
+};
+
+export const IMAGE = {
+    EQUILIBRIUM: ImageEquilibrium,
+    AVATAR: ImageAvatar
+};
