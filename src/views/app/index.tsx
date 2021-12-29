d79011a28b64ac3ce43ae12759a14392ad158f3a
david.monz <david.monz@macio.de>
2021-12-29 16:39:09 +0100

    Display NftCard in app

--- src/views/app/index.tsx
+++ src/views/app/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import './style.css';
+import { NftCard } from '../../components';
+import { IMAGE } from '../../assets/images';
 
 export default function App() {
-    return <div>Hello World!</div>;
+    return (
+        <div className={'app'}>
+            <NftCard
+                item={{
+                    id: 3429,
+                    image: IMAGE.EQUILIBRIUM,
+                    createdBy: { name: 'Jules Wyvern', avatar: IMAGE.AVATAR },
+                    name: 'Equilibrium',
+                    description: 'Our Equilibrium collection promotes balance and calm.',
+                    remainingTime: new Date(),
+                    value: 0.041
+                }}
+            />
+        </div>
+    );
 }
